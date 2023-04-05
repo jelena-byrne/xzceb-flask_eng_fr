627366a173dbd9714294568c93a458f5254603f2
Jelena Byrne <82006716+jelena-byrne@users.noreply.github.com>
2023-04-04 19:20:42 -0500

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -15,11 +15,13 @@ lt.set_service_url(url)
 
 def englishToFrench(englishText):
     # a function to translate English to French
+    englishText = input("Please enter text you would like to translate to French: /n")
     frenchText = lt.translate(text=englishText, model_id='en-fr').get_result()
     return frenchText
 
 def frenchToEnglish(frenchText):
     # a function to translate French to English
+    frenchText = input("Please enter text you would like to translate to English: /n")
     englishText = lt.translate(text=frenchText, model_id='fr-en').get_result()
     return englishText
 
