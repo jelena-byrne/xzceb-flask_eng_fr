0b7d22e4d4b2dc94380a56ea1749e4482b6ea807
Jelena Byrne <82006716+jelena-byrne@users.noreply.github.com>
2023-04-04 20:12:26 -0500

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,26 +9,25 @@ load_dotenv()
 apikey = os.environ['apikey']
 url = os.environ['url']
 
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
 authenticator = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(
-    version='{version??}',
-    authenticator=authenticator
-)
-
+language_translator = LanguageTranslatorV3(version='2023-04-04', authenticator=authenticator)
 language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com')
 
 def englishToFrench(englishText):
     # a function to translate English to French
-    englishText = input("Please enter text you would like to translate to French: /n")
     frenchText = language_translator.translate(text=englishText, model_id='en-fr').get_result()
     return frenchText
 
 def frenchToEnglish(frenchText):
     # a function to translate French to English
-    frenchText = input("Please enter text you would like to translate to English: /n")
     englishText = language_translator.translate(text=frenchText, model_id='fr-en').get_result()
     return englishText
 
+def main():
+    englishText = input("Please enter text you would like to translate to French:")
+    print(englishToFrench(englishText))
+    frenchText = input("Please enter text you would like to translate to English:")
+    print(frenchToEnglish(frenchText))
+
+if __name__ == "__main__":
+    main()
