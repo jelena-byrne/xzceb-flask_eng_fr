1a72615d8b2b4d4621dbf4ce631966ee3b226bc4
jelena-byrne <jelena.byrne@gmail.com>
2023-04-07 12:45:24 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,9 @@
-import json
+""" translation program using Python and IBM Watson Language Translator"""
+
+# import json
+import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,38 +14,37 @@ url = os.environ['url']
 # initiating the Watson Language Translator instance:
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='2018-05-01', authenticator=authenticator)
+    version='2023-04-07', authenticator=authenticator)
 language_translator.set_service_url(
     'https://api.us-south.language-translator.watson.cloud.ibm.com')
 
 
 def englishToFrench(englishText):
+    """ a function to translate English to French """
     if englishText is None:
         # handling empty input from user:
         return None
-    else:
-        # a function to translate English to French
-        fr_translated_text = language_translator.translate(
-            text=englishText, model_id='en-fr').get_result()
-        # isolate the written translation in the dictionary object returned
-        frenchText = fr_translated_text["translations"][0]["translation"]
-        return frenchText
+    fr_translated_text = language_translator.translate(
+        text=englishText, model_id='en-fr').get_result()
+    # isolate the written translation in the dictionary object returned
+    frenchText = fr_translated_text["translations"][0]["translation"]
+    return frenchText
 
 
 def frenchToEnglish(frenchText):
+    """ a function to translate French to English """
     if frenchText is None:
         # handling empty input from user:
         return None
-    else:
-        # a function to translate French to English
-        en_translated_text = language_translator.translate(
-            text=frenchText, model_id='fr-en').get_result()
-        # isolate the written translation in the dictionary object returned
-        englishText = en_translated_text["translations"][0]["translation"]
-        return englishText
+    en_translated_text = language_translator.translate(
+        text=frenchText, model_id='fr-en').get_result()
+    # isolate the written translation in the dictionary object returned
+    englishText = en_translated_text["translations"][0]["translation"]
+    return englishText
 
 
 def main():
+    """ main function to call on translation functions listed above """
     # Capture the text user wants to translate:
     englishText = input(
         "Please enter text you would like to translate to French: ")
